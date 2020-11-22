57f74cc3380b8bb8acc1f61fa2695d738ab449fe
Yoshiaki Togami <tuabobo123@gmail.com>
2020-11-23 00:53:38 +0900

    Add giyhub calender

---
+++ components/profile/Github.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
+import GithubCalender from "github-calendar";
+export const GithubGraph = () => {
+  const ref = useRef(null);
+  useEffect(() => {
+    GithubCalender(ref.current, "togami2864", {
+      responsive: true,
+      tooltips: true,
+    });
+  });
+  return (
+    <Calender>
+      <div className="calender" ref={ref}>
+        Loading the data just for you.
+      </div>
+    </Calender>
+  );
+};
+
+const Calender = styled.div`
+  span {
+    color: white !important;
+  }
+`;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,13 +1,24 @@
+import Head from "next/head";
 import styled from "styled-components";
 import { ProfileIcon } from "../components/profile/ProfileIcon";
 import { Introduction } from "../components/profile/Introduction";
+import { GithubGraph } from "../components/profile/Github";
 
 export default function profile({}) {
   return (
-    <Profile>
-      <ProfileIcon />
-      <Introduction />
-    </Profile>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"
+        />
+      </Head>
+      <Profile>
+        <ProfileIcon />
+        <Introduction />
+        <GithubGraph />
+      </Profile>
+    </>
   );
 }
 
