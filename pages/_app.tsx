3defb51600a62bcd3c8e84b84fe3e590b75c049a
Yoshiaki Togami <tuabobo123@gmail.com>
2020-12-06 14:01:54 +0900

    Add Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,21 @@ import Layout from "../components/Layout";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../assets/js/styles/theme";
 import { GlobalStyle } from "../assets/js/styles/global";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Main, NextScript, Head } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -31,6 +32,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Alata&display=swap"
