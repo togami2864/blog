c89d7817db80031cc3b0cca3df42c60e64af089e
Yoshiaki Togami <tuabobo123@gmail.com>
2020-12-06 13:29:45 +0900

    Fixed style in GitHubGraph

--- components/profile/Github.tsx
+++ components/profile/Github.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import styled from "styled-components";
+import media from "styled-media-query";
 const ReactGitHubCalendar = dynamic(() => import("react-ts-github-calendar"), {
   ssr: false,
 });
 export const GithubGraph = () => {
   return (
     <Calender>
-      <ReactGitHubCalendar userName="togami2864" tooltips />
+      <ReactGitHubCalendar userName="togami2864" tooltips responsive />
     </Calender>
   );
 };
@@ -16,4 +17,12 @@ const Calender = styled.div`
   span {
     color: white !important;
   }
+  .contrib-footer {
+    padding: 30px 0 !important;
+  }
+  ${media.lessThan("medium")`
+    .calendar-graph {
+      height: calc((42/414)*100vw);
+    }
+  `}
 `;
