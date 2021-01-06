b00f9738aa31240c6b92c5928f88c61e48430f24
Yoshiaki Togami <tuabobo123@gmail.com>
2021-01-06 21:01:25 +0900

    Add interest section

--- components/profile/Interest.tsx
+++ components/profile/Interest.tsx
@@ -3,7 +3,14 @@ export const Interest = () => {
   return (
     <InterestStyle>
       <h1>Interest</h1>
-      <p></p>
+      <Content>
+        I study an Electric Circuit and a Semiconductor Engineering at an
+        university.But, I also study about web programming and a computer, and
+        work as a frontend engineer. Especially,
+        <Strong> performance tuning </Strong> and
+        <Strong> ecosystem around JavaScript </Strong>
+        arose my curiosity !!
+      </Content>
     </InterestStyle>
   );
 };
@@ -11,4 +18,14 @@ export const Interest = () => {
 const InterestStyle = styled.div`
   padding-top: 20px;
   border-top: 1px solid white;
+  text-align: left;
+`;
+
+const Content = styled.p`
+  font-size: 1rem;
+  line-height: 1.2;
+`;
+
+const Strong = styled.strong`
+  color: greenyellow;
 `;
