a9f7d14016aa54ce108316cc0e783c118376273c
Yoshiaki Togami <tuabobo123@gmail.com>
2021-05-07 21:54:25 +0900

    feat: disable SSR

--- pages/oss.tsx
+++ pages/oss.tsx
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import jsdom from "jsdom";
 const { JSDOM } = jsdom;
+
+interface metaData {
+  link: string;
+  number: number;
+  title: string;
+  image: string;
+}
+
 export default function OSS({ cardDatas }) {
   return (
     <div>
@@ -8,6 +16,7 @@ export default function OSS({ cardDatas }) {
       <PRList>
         {cardDatas.map((data, index) => (
           <LinkToPr key={index} href={data.url} target="_blank">
+            <PrTitle>{`・#${data.number}:${data.title}`}</PrTitle>
             <Image src={data.image} />
           </LinkToPr>
         ))}
@@ -16,7 +25,7 @@ export default function OSS({ cardDatas }) {
   );
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const githubUrl =
     "https://api.github.com/search/issues?q=is:pr+repo:aws-amplify/amplify-cli+author:togami2864";
   const pullRequests = await fetch(githubUrl, {
@@ -27,12 +36,16 @@ export const getServerSideProps = async () => {
   }).then((res) => res.json());
   const pullRequestUrls = [];
   pullRequests.items.map((item) => {
-    pullRequestUrls.push(item.html_url);
+    pullRequestUrls.push({
+      link: item.html_url,
+      number: item.number,
+      title: item.title,
+    });
   });
 
   const temps = await Promise.all(
-    pullRequestUrls.map(async (link: string) => {
-      const metas = await fetch(link, {
+    pullRequestUrls.map(async (item: metaData) => {
+      const metas = await fetch(item.link, {
         method: "GET",
         headers: {
           Authorization: process.env.GITHUB_TOKEN,
@@ -41,7 +54,9 @@ export const getServerSideProps = async () => {
         .then((res) => res.text())
         .then((text) => {
           const metaData = {
-            url: link,
+            url: item.link,
+            number: item.number,
+            title: item.title,
             image: "",
           };
           const doms = new JSDOM(text);
@@ -75,12 +90,21 @@ const PRList = styled.ul`
   align-items: center;
   justify-content: space-around;
   flex-wrap: wrap;
+  a {
+    color: white;
+  }
+  a:visited {
+    color: yellow;
+  }
 `;
 
 const LinkToPr = styled.a`
   display: block;
   width: 400px;
   margin: 20px;
+`;
+const PrTitle = styled.p`
+  display: block;
 `;
 
 const Image = styled.img`
